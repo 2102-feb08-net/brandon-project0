36d4af86dc61d7ef269d38a3d77520cbff774da4
Exsolutus <exsolutus.dev@gmail.com>
2021-02-16 12:21:00 -0500

    Setup solution with Core, UI, and Test projects

---
+++ Project0.Core/src/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project0.Core
+{
+    public class Startup
+    {
+        public void Main()
+        {
+            
+        }
+    }
+}
---
+++ Project0.UI/Project0.UI.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Project0.UI
+{
+    public class Class1
+    {
+    }
+}
